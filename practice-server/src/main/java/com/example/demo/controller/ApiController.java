995d16dc862de1b6a40ae340f6ed3e422786f223
조준희 <@192.168.219.105>
2024-09-16 22:28:31 +0900

    redis 설정

--- practice-server/src/main/java/com/example/demo/controller/ApiController.java
+++ practice-server/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,10 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class ApiController {
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
 	
 	@GetMapping("/")
 	public String init() {
@@ -14,5 +23,36 @@ public class ApiController {
 	@GetMapping("/test")
 	public String test() {
 		return "just testing...!!";
+	}
+	
+	@GetMapping("/country/code/{code}")
+	public String printCodeInfo(@PathVariable int code) {
+		COUNTRY country = COUNTRY.AMERICA;		
+		return String.format("국가코드: %d", country.code);
+	}
+	
+	@GetMapping("/update/cache")
+	public String updateCacheData() {
+		redisTemplate.opsForValue().set("name", "junhee");
+		return "";
+	}
+	
+	@GetMapping("/get/cache")
+	public String getCacheData() {
+		String result = redisTemplate.opsForValue().get("name");
+		return result;
+	}
+}
+
+enum COUNTRY {
+	KOREA(1),
+	AMERICA(2),
+	JAPAN(3),
+	CHINA(4);
+	
+	int code;
+
+	COUNTRY(int code) {
+		this.code = code;
 	}
 }
