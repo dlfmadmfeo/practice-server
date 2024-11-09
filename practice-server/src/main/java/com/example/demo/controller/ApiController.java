b2f2f399437e716924454e07c7fd61571c7fc77b
조준희 <@192.168.219.105>
2024-11-09 19:35:26 +0900

    수정

--- practice-server/src/main/java/com/example/demo/controller/ApiController.java
+++ practice-server/src/main/java/com/example/demo/controller/ApiController.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
@@ -22,7 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @CrossOrigin(origins={"http://localhost:5173", "http://junhee92kr.com"})
-@RequestMapping("/api") 
 public class ApiController {
 	protected void show() {}
 	@Autowired
