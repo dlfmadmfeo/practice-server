8b6567ea0841ca0f8442cce539fb63102c16d2df
junhee <dlfmadmfeo@gmail.com>
2025-07-31 16:03:16 +0900

    Options 메서드 허용 옵션 추가

--- practice-server/src/main/java/com/example/demo/config/SecurityConfig.java
+++ practice-server/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(auth -> auth
+        		.requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .requestMatchers(
                 		"/**"
 //                    "/swagger-ui/**",
